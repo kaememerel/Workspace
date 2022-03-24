e97d508218d68842d2cad33f25e0f4edd4cbcd43
Katarzyna <102320637+kaememerel@users.noreply.github.com>
2022-03-24 21:08:05 +0000

    Add files via upload

---
+++ tutorial1_formatowanie_napisow.py
@@ -0,0 +1,22 @@
+imie = "Kasia"
+print("Witaj, %s!" % imie)
+#imie = "Marek"
+wiek = 32
+print("%s ma %d lata." % (imie, wiek))
+MojaTab = [1,2,3]
+print("Tablica: %s" % MojaTab)
+liczba = 4
+print("liczba = %d" % liczba)
+print("imie = %s" % imie)
+#print("imie = %d" % imie)
+dane = ("Dorota", 5, 32)
+print("%s mieszka w bloku nr %d w mieszkaniu %d" % dane)
+print("====przyklad====")
+rzeczywista_1 = 4.34
+rzeczywista_2 = 54.432
+calkowita = 16
+
+print ("rzeczywista_1 = %f" % rzeczywista_1)
+print ("rzeczywista_2 = %f" % rzeczywista_2)
+print ("rzeczywista_2 = %.1f" % rzeczywista_2)
+print ("W systemie szesnastkowym %d ma postac %X" % (calkowita, calkowita))
---
+++ tutorial1_operatory.py
@@ -0,0 +1,17 @@
+suma = 1 + 2 * 3 / 4.0
+print (suma)
+reszta = 11 % 3
+print(reszta)
+kwadrat = 7 ** 2
+szescian = 7 ** 3
+print(kwadrat, szescian)
+print("====operacje na napisach====")
+wielewitaj = "witaj " * 10
+print(wielewitaj)
+print("======operacje na tablicach====")
+parzyste_dodatnie = [2, 4, 6, 8]
+nieparzyste_dodatnie = [1, 3, 5, 7]
+naturalne = parzyste_dodatnie + nieparzyste_dodatnie
+print(naturalne)
+print ([1,2,3] * 3)
+print(naturalne * 2)
